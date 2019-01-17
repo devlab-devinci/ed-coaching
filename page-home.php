1f42726adca57f7cb575e352b14baa0d0cf0c25a
Alex.Landeau <alex.landeau@betc.com>
2019-01-17 19:49:51 +0100

    Homepage inte wireframe 0.1

--- page-home.php
+++ page-home.php
@@ -16,29 +16,56 @@
 
 get_header(); ?>
 
-	<div class="wrap">
 
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
-				<h1> Contenu page d'accueil </h1>
-                <hr>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <section class="home-element banner">
 
-                <!--Temoignage Section-->
-                <h2>Partie témoignage</h2>
-				<?php
-                    $args = array( 'post_type' => 'temoignage', 'posts_per_page' => 10 );
-                    $loop = new WP_Query( $args );
-                    while ( $loop->have_posts() ) : $loop->the_post();
-                        the_title();
-                        echo '<div class="entry-content">';
-                        the_content();
-                        echo '</div>';
-                    endwhile;
-				?>
+            </section>
+            <section class="home-element activity">
+                <div class="wrap">
+                    <aside>
+                        <div class="banner-title-content">
+                            <h2>Découvrez nos activités</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis nulla mattis, ultricies nunc fringilla, faucibus nisi. Aenean scelerisque tortor auctor, hendrerit orci id, ornare arcu. Sed tempor turpis eu enim pellentesque auctor. Fusce non hendrerit neque. Quisque a ex sem. In ultrices massa ac risus egestas, id rutrum nunc vehicula. </p>
+                        </div>
+                    </aside>
+                    <article>
+                        <ul class="banner-list-content">
+                            <li>
+                                <img src="" alt="pict 1">
+                                <h3>Booking</h3>
+                                <p>Cras quis urna quis nibh commodo molestie sit amet non nunc. Phasellus lacus purus, vestibulum in tincidunt vitae, elementum sit amet sem.</p>
+                            </li>
+                            <li>
+                                <img src="" alt="pict 2">
+                                <h3>Coaching</h3>
+                                <p>Cras quis urna quis nibh commodo molestie sit amet non nunc. Phasellus lacus purus, vestibulum in tincidunt vitae, elementum sit amet sem.</p>
+                            </li>
+                            <li>
+                                <img src="" alt="pict 3">
+                                <h3>Shopping</h3>
+                                <p>Cras quis urna quis nibh commodo molestie sit amet non nunc. Phasellus lacus purus, vestibulum in tincidunt vitae, elementum sit amet sem.</p>
+                            </li>
+                            <li>
+                                <img src="" alt="pict 4">
+                                <h3>Achieve</h3>
+                                <p>Cras quis urna quis nibh commodo molestie sit amet non nunc. Phasellus lacus purus, vestibulum in tincidunt vitae, elementum sit amet sem.</p>
+                            </li>
+                        </ul>
+                    </article>
+                </div>
+            </section>
+            <div class="home-element contact">
 
+            </div>
+            <section class="home-element coach">
 
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	</div><!-- .wrap -->
+            </section>
+            <section class="home-element partner">
+
+            </section>
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php get_footer();
