972d0fe0d8771c38a5100407947149243492616e
Alex.Landeau <landeau.alex@gmail.com>
2019-01-23 18:15:47 +0100

    Homepage inte wireframe 0.2

--- page-home.php
+++ page-home.php
@@ -23,15 +23,15 @@ get_header(); ?>
 
             </section>
             <section class="home-element activity">
-                <div class="wrap">
+                <div class="wrap-list">
                     <aside>
-                        <div class="banner-title-content">
+                        <div class="title-content">
                             <h2>Découvrez nos activités</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis nulla mattis, ultricies nunc fringilla, faucibus nisi. Aenean scelerisque tortor auctor, hendrerit orci id, ornare arcu. Sed tempor turpis eu enim pellentesque auctor. Fusce non hendrerit neque. Quisque a ex sem. In ultrices massa ac risus egestas, id rutrum nunc vehicula. </p>
                         </div>
                     </aside>
                     <article>
-                        <ul class="banner-list-content">
+                        <ul class="list-content">
                             <li>
                                 <img src="" alt="pict 1">
                                 <h3>Booking</h3>
@@ -57,13 +57,73 @@ get_header(); ?>
                 </div>
             </section>
             <div class="home-element contact">
-
+                <div class="wrap">
+                    <h3>Pour nous contacter rien de plus simple</h3>
+                    <button>Contactez-nous</button>
+                </div>
             </div>
             <section class="home-element coach">
-
+                <div class="wrap-list">
+                    <aside>
+                        <div class="title-content">
+                            <h2>Nos Coachs</h2>
+                        </div>
+                    </aside>
+                    <article>
+                        <ul class="list-content">
+                            <li>
+                                <img src="" alt="pict 1">
+                                <h3>Edouard</h3>
+                                <p>Cras quis urna quis nibh commodo molestie sit amet non nunc. Phasellus lacus purus, vestibulum in tincidunt vitae, elementum sit amet sem.</p>
+                            </li>
+                            <li>
+                                <img src="" alt="pict 2">
+                                <h3>Gauthier</h3>
+                                <p>Cras quis urna quis nibh commodo molestie sit amet non nunc. Phasellus lacus purus, vestibulum in tincidunt vitae, elementum sit amet sem.</p>
+                            </li>
+                            <li>
+                                <img src="" alt="pict 3">
+                                <h3>Remi</h3>
+                                <p>Cras quis urna quis nibh commodo molestie sit amet non nunc. Phasellus lacus purus, vestibulum in tincidunt vitae, elementum sit amet sem.</p>
+                            </li>
+                            <li>
+                                <img src="" alt="pict 4">
+                                <h3>Remi</h3>
+                                <p>Cras quis urna quis nibh commodo molestie sit amet non nunc. Phasellus lacus purus, vestibulum in tincidunt vitae, elementum sit amet sem.</p>
+                            </li>
+                            <li>
+                                <img src="" alt="pict 5">
+                                <h3>Remi</h3>
+                                <p>Cras quis urna quis nibh commodo molestie sit amet non nunc. Phasellus lacus purus, vestibulum in tincidunt vitae, elementum sit amet sem.</p>
+                            </li>
+                        </ul>
+                    </article>
+                </div>
             </section>
             <section class="home-element partner">
-
+                <div class="wrap-list">
+                    <aside>
+                        <div class="title-content">
+                            <h2>Nos Partenaires</h2>
+                        </div>
+                    </aside>
+                    <article>
+                        <ul class="list-content">
+                            <li>
+                                <img src="" alt="pict 1">
+                                <h3>Barki's talent</h3>
+                            </li>
+                            <li>
+                                <img src="" alt="pict 2">
+                                <h3>Partenaires 2</h3>
+                            </li>
+                            <li>
+                                <img src="" alt="pict 3">
+                                <h3>Nu Skin</h3>
+                            </li>
+                        </ul>
+                    </article>
+                </div>
             </section>
         </main><!-- #main -->
     </div><!-- #primary -->
